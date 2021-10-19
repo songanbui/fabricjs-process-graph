0a3b9d2d7ccd8d79ba64f1a6bbe37c0aee27a1f8
Song An BUI <songan.bui@gmail.com>
2021-10-19 14:09:47 +0900

    serve demo/app.js with express

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
+# Requirements
+
+`bower` and `grunt-cli` must be installed globally.
+
+- npm install bower -g
+- npm install grunt-cli -g
+
+# Build
+
 - npm install
 - bower install
 - grunt
-- open demo/index.html in your browser
+  
+# Run demo
+
+To try out the demo, serve the `demo` folder as an HTML application using Express.
+
+- npm run demo
+- open http://localhost:300/demo/index.html in your browser
---
+++ demo/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/demo', express.static(path.join(__dirname)));
+app.use('/index.js', express.static(path.join(__dirname, '/../index.js')));
+app.use('/bower_components', express.static(path.join(__dirname, '/../bower_components')));
+app.use('/dist', express.static(path.join(__dirname, '/../dist')));
+
+// error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(3000, () => {
+  console.log('Demo app listening on port 3000!');
+});
+
+module.exports = app;
